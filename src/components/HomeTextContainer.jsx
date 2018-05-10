28679253b1f46b80801bfe574e4fbc0af2059422
grabeqx <grabowski.kontakt@gmail.com>
2018-05-10 13:19:34 +0200

    Add SenatorPage component

--- src/components/HomeTextContainer.jsx
+++ src/components/HomeTextContainer.jsx
@@ -14,8 +14,7 @@ class HomeTextContainer extends React.Component {
                     Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. 
                     Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. 
                     Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, 
-                    a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, 
-                    jak Aldus PageMaker</p>
+                    a ostatnio z zawierającym różne wersje</p>
                 <p className="criteria-link-container">
                     <Link to="#">Nasze kryteria <img src="/images/arror-right-green.png" /></Link>
                 </p>
---
+++ src/components/SenatorPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Search from './Search';
+
+class SenatorPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default SenatorPage;
